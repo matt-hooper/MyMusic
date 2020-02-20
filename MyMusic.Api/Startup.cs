e56e98e298964afd130263b7786f4264d9be2c41
Matt Hooper <matt.hooper@outlook.com>
2020-02-20 21:30:13 +0000

    Add Data repositories

--- MyMusic.Api/Startup.cs
+++ MyMusic.Api/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MyMusic.Core;
+using MyMusic.Data;
 
 namespace MyMusic.Api
 {
@@ -26,6 +29,8 @@ namespace MyMusic.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddDbContext<MyMusicDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default"), x => x.MigrationsAssembly("MyMusic.Data")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
